e2e99516f02dd2742c2637810de6bcdac6d0d8a3
Brandon L <20brandonl02@gmail.com>
2025-07-08 15:24:26 -0400

    added form component

--- proj3/src/App.jsx
+++ proj3/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Form from "./components/Form.jsx"
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,12 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Form />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
